bf8a16319b3e11f583f31145a19f136466f568f9
kwiecien <k.d.kwiecinski@gmail.com>
2018-06-18 00:13:32 +0200

    feat: Display teams in a fragment

--- app/src/main/java/com/kk/worldcup2018/view/MainActivity.java
+++ app/src/main/java/com/kk/worldcup2018/view/MainActivity.java
@@ -1,33 +1,36 @@
-package com.kk.worldcup2018;
+package com.kk.worldcup2018.view;
 
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
-import android.view.MenuItem;
+
+import com.kk.worldcup2018.R;
+
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_teams:
-                    return true;
-                case R.id.navigation_fixtures:
-                    return true;
-                case R.id.navigation_groups:
-                    return true;
-                default:
-            }
-            return false;
+            = item -> {
+        switch (item.getItemId()) {
+            case R.id.navigation_teams:
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.main_fragment, TeamsFragment.newInstance(1))
+                        .commit();
+                return true;
+            case R.id.navigation_fixtures:
+                return true;
+            case R.id.navigation_groups:
+                return true;
+            default:
         }
+        return false;
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Timber.plant(new Timber.DebugTree());
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
---
+++ app/src/main/java/com/kk/worldcup2018/view/TeamsFragment.java
@@ -0,0 +1,156 @@
+package com.kk.worldcup2018.view;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.kk.worldcup2018.R;
+import com.kk.worldcup2018.data.FootballFetcher;
+import com.kk.worldcup2018.model.Team;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+import hugo.weaving.DebugLog;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import timber.log.Timber;
+
+/**
+ * A fragment representing a list of Items.
+ * <p/>
+ * Activities containing this fragment MUST implement the {@link OnListFragmentInteractionListener}
+ * interface.
+ */
+public class TeamsFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    private final WorldCupFetcher worldCupFetcher = new WorldCupFetcher();
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private OnListFragmentInteractionListener mListener;
+    private RecyclerView recyclerView;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public TeamsFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    public static TeamsFragment newInstance(int columnCount) {
+        TeamsFragment fragment = new TeamsFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_teams_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new TeamsRecyclerViewAdapter(new ArrayList<>(), mListener));
+        }
+
+        worldCupFetcher.getTeams();
+
+        return view;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnListFragmentInteractionListener) {
+            //    mListener = (OnListFragmentInteractionListener) context;
+        } else {
+            //      throw new RuntimeException(context.toString()
+            //              + " must implement OnListFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        //  mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnListFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onListFragmentInteraction(Team item);
+    }
+
+    public class WorldCupFetcher {
+
+        private static final String WORLD_CUP_API_ENDPOINT = "http://api.football-data.org/v1/competitions/467/";
+        private final FootballFetcher fetcher = new Retrofit.Builder()
+                .baseUrl(WORLD_CUP_API_ENDPOINT)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(FootballFetcher.class);
+
+        @DebugLog
+        public List<Team> getTeams() {
+            final List<Team> teams = new ArrayList<>();
+            fetcher.getTeams().enqueue(new Callback<FootballFetcher.TeamsResponse>() {
+                @Override
+                public void onResponse(Call<FootballFetcher.TeamsResponse> call, Response<FootballFetcher.TeamsResponse> response) {
+                    List<Team> teams = response.body().getTeams();
+                    teams.sort(Comparator.comparing(Team::getName));
+                    ((TeamsRecyclerViewAdapter) recyclerView.getAdapter()).setTeams(teams);
+                    recyclerView.getAdapter().notifyDataSetChanged();
+                }
+
+                @Override
+                public void onFailure(Call<FootballFetcher.TeamsResponse> call, Throwable t) {
+                    Timber.e(t);
+                }
+            });
+            return teams;
+        }
+
+    }
+
+}
