0211a81f85e6463273723f9824421cee973d3b81
kwiecien <k.d.kwiecinski@gmail.com>
2018-06-20 23:47:18 +0200

    fix: Fix bug when switching fragments by changing Fragment to FrameView

--- app/src/main/java/com/kk/worldcup2018/view/TeamsFragment.java
+++ app/src/main/java/com/kk/worldcup2018/view/TeamsFragment.java
@@ -2,6 +2,7 @@ package com.kk.worldcup2018.view;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
@@ -68,7 +69,7 @@ public class TeamsFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_teams_list, container, false);
         setupRecyclerView(view);
