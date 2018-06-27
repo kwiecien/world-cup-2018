9cfc89b9f6f3e3837a20c1bd26b720e9082f55dd
kwiecien <k.d.kwiecinski@gmail.com>
2018-06-27 19:26:49 +0200

    fix: Fix nullable context in ContextCompat

--- app/src/main/java/com/kk/worldcup2018/view/support/RecyclerViewUtils.java
+++ app/src/main/java/com/kk/worldcup2018/view/support/RecyclerViewUtils.java
@@ -2,6 +2,8 @@ package com.kk.worldcup2018.view.support;
 
 import android.content.Context;
 import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.RecyclerView;
 
@@ -13,7 +15,10 @@ public class RecyclerViewUtils {
         throw new UnsupportedOperationException("Suppress default constructor for noninstantiability");
     }
 
-    public static void addDividerToRecyclerView(Context context, RecyclerView recyclerView) {
+    public static void addDividerToRecyclerView(@Nullable Context context, @NonNull RecyclerView recyclerView) {
+        if (context == null) {
+            return;
+        }
         Drawable divider = ContextCompat.getDrawable(context, R.drawable.divider);
         recyclerView.addItemDecoration(new DividerItemDecoration(divider));
     }
