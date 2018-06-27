dbd7993000dc7599e80483b2f17b2f3927b7f7bd
kwiecien <k.d.kwiecinski@gmail.com>
2018-06-27 19:30:30 +0200

    feat: Add api keys

---
+++ app/src/main/java/com/kk/worldcup2018/data/ApiKeys.java
@@ -0,0 +1,5 @@
+package com.kk.worldcup2018.data;
+
+public class ApiKeys {
+    static final String FOOTBALL_DATA_ORG = "your-api-key";
+}
--- app/src/main/java/com/kk/worldcup2018/data/FootballFetcher.java
+++ app/src/main/java/com/kk/worldcup2018/data/FootballFetcher.java
@@ -6,15 +6,19 @@ import com.kk.worldcup2018.data.response.TeamsResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Headers;
 
 public interface FootballFetcher {
 
+    @Headers("X-Auth-Token: " + ApiKeys.FOOTBALL_DATA_ORG)
     @GET("teams")
     Call<TeamsResponse> getTeams();
 
+    @Headers("X-Auth-Token: " + ApiKeys.FOOTBALL_DATA_ORG)
     @GET("fixtures")
     Call<FixturesResponse> getFixtures();
 
+    @Headers("X-Auth-Token: " + ApiKeys.FOOTBALL_DATA_ORG)
     @GET("leagueTable")
     Call<StandingsResponse> getGroups();
 
