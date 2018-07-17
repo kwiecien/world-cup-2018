a7c61adc158ae7d2da435bd83c7b17d1df19c730
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-17 21:13:34 +0200

    feat: Open app when clicking on widget

--- app/src/main/java/com/kk/worldcup2018/view/widget/FavoriteTeamWidget.java
+++ app/src/main/java/com/kk/worldcup2018/view/widget/FavoriteTeamWidget.java
@@ -1,11 +1,14 @@
 package com.kk.worldcup2018.view.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 
 import com.kk.worldcup2018.R;
+import com.kk.worldcup2018.view.MainActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -15,8 +18,13 @@ public class FavoriteTeamWidget extends AppWidgetProvider {
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favorite_team_widget);
-        views.setTextViewText(R.id.appwidget_team, "Poland"); // TODO
-        views.setTextViewText(R.id.appwidget_group, "Group H"); // TODO
+        views.setTextViewText(R.id.widget_team, "Poland"); // TODO
+        views.setTextViewText(R.id.widget_group, "Group H"); // TODO
+
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        views.setOnClickPendingIntent(R.id.widget_layout, pendingIntent);
+
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
