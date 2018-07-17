e964cb5384098c8e131a38152fb56021d6990924
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-17 20:59:31 +0200

    feat: Create first sketch of the widget

--- app/src/main/java/com/kk/worldcup2018/view/widget/FavoriteTeamWidget.java
+++ app/src/main/java/com/kk/worldcup2018/view/widget/FavoriteTeamWidget.java
@@ -1,9 +1,11 @@
-package com.kk.worldcup2018;
+package com.kk.worldcup2018.view.widget;
 
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.widget.RemoteViews;
+
+import com.kk.worldcup2018.R;
 
 /**
  * Implementation of App Widget functionality.
@@ -12,19 +14,14 @@ public class FavoriteTeamWidget extends AppWidgetProvider {
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.favorite_team_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
-
-        // Instruct the widget manager to update the widget
+        views.setTextViewText(R.id.appwidget_team, "Poland"); // TODO
+        views.setTextViewText(R.id.appwidget_group, "Group H"); // TODO
         appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
@@ -32,7 +29,7 @@ public class FavoriteTeamWidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
-        // Enter relevant functionality for when the first widget is created
+        // Enter relevant functionality for when the first widget is created // TODO
     }
 
     @Override
