7223c8f73e15b9c7c161ca28c7a671ab66c39387
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-19 22:53:43 +0200

    feat: Set up Firebase in gradle and manifest

---
+++ app/src/main/java/com/kk/worldcup2018/firebase/MyFirebaseMessagingService.java
@@ -0,0 +1,7 @@
+package com.kk.worldcup2018.firebase;
+
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+
+}
