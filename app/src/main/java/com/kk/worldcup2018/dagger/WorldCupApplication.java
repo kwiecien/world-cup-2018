e78fa16bbb33a2ddc67b9a331f3b9012efde8df9
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-19 19:46:09 +0200

    feat: Add Google Analytics to Application

--- app/src/main/java/com/kk/worldcup2018/dagger/WorldCupApplication.java
+++ app/src/main/java/com/kk/worldcup2018/dagger/WorldCupApplication.java
@@ -3,20 +3,33 @@ package com.kk.worldcup2018.dagger;
 import android.app.Application;
 
 import com.facebook.stetho.Stetho;
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+import com.kk.worldcup2018.R;
 
 public class WorldCupApplication extends Application {
 
+    private static GoogleAnalytics googleAnalytics;
+    private static Tracker tracker;
     private WorldCupComponent worldCupComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
         Stetho.initializeWithDefaults(this);
+        googleAnalytics = GoogleAnalytics.getInstance(this);
         worldCupComponent = createWorldCupComponent();
     }
 
     public WorldCupComponent getWorldCupComponent() {
         return worldCupComponent;
+    }
+
+    public synchronized Tracker getTracker() {
+        if (tracker == null) {
+            tracker = googleAnalytics.newTracker(R.xml.global_tracker);
+        }
+        return tracker;
     }
 
     private WorldCupComponent createWorldCupComponent() {
