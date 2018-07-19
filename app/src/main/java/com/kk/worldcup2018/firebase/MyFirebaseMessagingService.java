278585a2b39b087c4b6806e624f80c17a7c1ce6c
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-19 23:12:48 +0200

    feat: Generate new Firebase token

--- app/src/main/java/com/kk/worldcup2018/firebase/MyFirebaseMessagingService.java
+++ app/src/main/java/com/kk/worldcup2018/firebase/MyFirebaseMessagingService.java
@@ -2,6 +2,17 @@ package com.kk.worldcup2018.firebase;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 
+import timber.log.Timber;
+
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
+    /**
+     * Called if InstanceID token is updated. This may occur if the security of
+     * the previous token had been compromised. Note that this is called when the InstanceID token
+     * is initially generated so this is where you would retrieve the token.
+     */
+    @Override
+    public void onNewToken(String token) {
+        Timber.d("Refreshed token: %s", token);
+    }
 }
