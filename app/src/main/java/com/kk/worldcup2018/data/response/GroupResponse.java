c05313fa97cd49f89ee86751bb0c5827623c5588
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-21 19:59:26 +0200

    feat: Remove problematic Lombok

--- app/src/main/java/com/kk/worldcup2018/data/response/GroupResponse.java
+++ app/src/main/java/com/kk/worldcup2018/data/response/GroupResponse.java
@@ -1,8 +1,5 @@
 package com.kk.worldcup2018.data.response;
 
-import lombok.Data;
-
-@Data
 class GroupResponse {
     private final String group;
     private final int rank;
@@ -13,4 +10,52 @@ class GroupResponse {
     private final int goals;
     private final int goalsAgainst;
     private final int goalDifference;
+
+    public GroupResponse(String group, int rank, String team, int teamId, int playedGames, int points, int goals, int goalsAgainst, int goalDifference) {
+        this.group = group;
+        this.rank = rank;
+        this.team = team;
+        this.teamId = teamId;
+        this.playedGames = playedGames;
+        this.points = points;
+        this.goals = goals;
+        this.goalsAgainst = goalsAgainst;
+        this.goalDifference = goalDifference;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public String getTeam() {
+        return team;
+    }
+
+    public int getTeamId() {
+        return teamId;
+    }
+
+    public int getPlayedGames() {
+        return playedGames;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public int getGoals() {
+        return goals;
+    }
+
+    public int getGoalsAgainst() {
+        return goalsAgainst;
+    }
+
+    public int getGoalDifference() {
+        return goalDifference;
+    }
 }
