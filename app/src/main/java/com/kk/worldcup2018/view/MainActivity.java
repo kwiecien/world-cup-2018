a1deb5dec7d890a6bb732ebeb173c04ab500c7b9
kwiecien <k.d.kwiecinski@gmail.com>
2018-07-23 23:16:20 +0200

    feat: Remove problematic Explode transition

--- app/src/main/java/com/kk/worldcup2018/view/MainActivity.java
+++ app/src/main/java/com/kk/worldcup2018/view/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
-import android.transition.Explode;
 import android.transition.Slide;
 import android.view.Gravity;
 import android.view.MenuItem;
@@ -56,16 +55,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        setUpTransitions();
         setUpBottomNavigation();
         if (savedInstanceState == null) {
             removeSelection(navigation);
             setWelcomeScreen();
         }
-    }
-
-    private void setUpTransitions() {
-        getWindow().setExitTransition(new Explode());
     }
 
     private void setUpBottomNavigation() {
